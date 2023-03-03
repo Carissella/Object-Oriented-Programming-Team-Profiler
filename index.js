5807ad8dc466e5f09945e96ae5102948aa623704
Carissella <carissebarrswe@gmail.com>
2023-03-03 01:17:56 -0500

    created JS file and variables

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require("inquirer");
+const fs = require ("fs");
+const path = require("path");
+const OUTPUT_DIR = path.resolve();
+const outputPath = path.join(OUTPUT_DIR, "index.html");
+const generateTeams = require();
+
+const Employee = require();
+const Manager = require();
+const Engineer = require();
+const Intern = require();
+
+
+const teamsMembers = [];
+
